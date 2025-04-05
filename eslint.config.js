42387c98a950cc454aefb654ea8df6c6ed882b58
ErgalignIntel <Dhampton89.dh@gmail.com>
2025-04-05 03:38:54 -0400

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+// eslint.config.js
+// Basic ESLint configuration using recommended rules
+
+import globals from "globals";
+import js from "@eslint/js";
+
+export default [
+  // Use ESLint's recommended default rules
+  js.configs.recommended,
+
+  // Add some global variables common in browser scripts
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser // Allows things like 'document', 'window', 'alert' etc.
+      }
+    },
+    rules: {
+      // You can customize rules here later if needed.
+      // For example, to just warn about unused variables instead of causing an error:
+      // "no-unused-vars": "warn"
+    }
+  }
+];
